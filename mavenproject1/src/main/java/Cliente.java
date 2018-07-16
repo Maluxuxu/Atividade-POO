742660f46d8c28c0f4c2648f7b3527dc9eee891d
Maluxuxu <larlu1466@gmail.com>
2018-07-16 19:01:28 -0300

    oiiii

---
+++ mavenproject1/src/main/java/Cliente.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+    private String codidentificaco;
+    private String email;
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String tipoCliente;
+
+    /**
+     * @return the codidentificaco
+     */
+    public String getCodidentificaco() {
+        return codidentificaco;
+    }
+
+    /**
+     * @param codidentificaco the codidentificaco to set
+     */
+    public void setCodidentificaco(String codidentificaco) {
+        this.codidentificaco = codidentificaco;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the nomeFantasia
+     */
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    /**
+     * @param nomeFantasia the nomeFantasia to set
+     */
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    /**
+     * @return the razaoSocial
+     */
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    /**
+     * @param razaoSocial the razaoSocial to set
+     */
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    /**
+     * @return the tipoCliente
+     */
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    /**
+     * @param tipoCliente the tipoCliente to set
+     */
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+    
+    
+}
